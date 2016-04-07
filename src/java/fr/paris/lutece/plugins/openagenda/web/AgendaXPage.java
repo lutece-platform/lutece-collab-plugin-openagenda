c683567b8b442a73e0d90cc63bf7f7a546cb5118
Jon Harper <jon.harper87@gmail.com>
2016-04-07 14:34:57 +0200

    OPENAGENDA-2: don't allow edition in FO

--- src/java/fr/paris/lutece/plugins/openagenda/web/AgendaXPage.java
+++ src/java/fr/paris/lutece/plugins/openagenda/web/AgendaXPage.java
@@ -35,213 +35,58 @@ package fr.paris.lutece.plugins.openagenda.web;
  
 import fr.paris.lutece.plugins.openagenda.business.Agenda;
 import fr.paris.lutece.plugins.openagenda.business.AgendaHome;
-import fr.paris.lutece.portal.util.mvc.commons.annotations.Action;
 import fr.paris.lutece.portal.web.xpages.XPage;
 import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
 import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
-import fr.paris.lutece.util.url.UrlItem;
 import java.util.Map;
-import fr.paris.lutece.portal.service.message.SiteMessageService;
-import fr.paris.lutece.portal.service.message.SiteMessage;
-import fr.paris.lutece.portal.service.message.SiteMessageException;
 import javax.servlet.http.HttpServletRequest; 
 
 /**
- * This class provides the user interface to manage Agenda xpages ( manage, create, modify, remove )
+ * This class provides the user interface to list all Agendas and view an agenda
  */
  
 @Controller( xpageName = "agenda" , pageTitleI18nKey = "openagenda.xpage.agenda.pageTitle" , pagePathI18nKey = "openagenda.xpage.agenda.pagePathLabel" )
 public class AgendaXPage extends MVCApplication
 {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 2893092282766092854L;
     // Templates
     private static final String TEMPLATE_MANAGE_AGENDAS="/skin/plugins/openagenda/manage_agendas.html";
-    private static final String TEMPLATE_CREATE_AGENDA="/skin/plugins/openagenda/create_agenda.html";
-    private static final String TEMPLATE_MODIFY_AGENDA="/skin/plugins/openagenda/modify_agenda.html";
-    private static final String TEMPLATE_LIST_EVENTS="/skin/plugins/openagenda/list_events.html";
     private static final String TEMPLATE_AGENDA_EVENTS="/skin/plugins/openagenda/agenda_events.html";
-    
-    // JSP
-    private static final String JSP_PAGE_PORTAL = "jsp/site/Portal.jsp";
     
     // Parameters
     private static final String PARAMETER_ID_AGENDA="id";
-    private static final String PARAM_ACTION = "action";
-    private static final String PARAM_PAGE = "page";
     
     // Markers
     private static final String MARK_AGENDA_LIST = "agenda_list";
     private static final String MARK_AGENDA = "agenda";
     
-    // Message
-    private static final String MESSAGE_CONFIRM_REMOVE_AGENDA = "openagenda.message.confirmRemoveAgenda";
-    
     // Views
     private static final String VIEW_MANAGE_AGENDAS = "manageAgendas";
-    private static final String VIEW_CREATE_AGENDA = "createAgenda";
-    private static final String VIEW_MODIFY_AGENDA = "modifyAgenda";
     private static final String VIEW_AGENDA_EVENTS = "agendaEvents";
     
-    // Actions
-    private static final String ACTION_CREATE_AGENDA = "createAgenda";
-    private static final String ACTION_MODIFY_AGENDA= "modifyAgenda";
-    private static final String ACTION_REMOVE_AGENDA = "removeAgenda";
-    private static final String ACTION_CONFIRM_REMOVE_AGENDA = "confirmRemoveAgenda";
-
-    // Infos
-    private static final String INFO_AGENDA_CREATED = "openagenda.info.agenda.created";
-    private static final String INFO_AGENDA_UPDATED = "openagenda.info.agenda.updated";
-    private static final String INFO_AGENDA_REMOVED = "openagenda.info.agenda.removed";
-    
-    // Session variable to store working values
-    private Agenda _agenda;
     
     @View( value = VIEW_MANAGE_AGENDAS, defaultView = true )
     public XPage getManageAgendas( HttpServletRequest request )
     {
-        _agenda = null;
         Map<String, Object> model = getModel(  );
         model.put( MARK_AGENDA_LIST, AgendaHome.getAgendasList(  ) );
 
         return getXPage( TEMPLATE_MANAGE_AGENDAS, request.getLocale(  ), model );
     }
 
-    /**
-     * Returns the form to create a agenda
-     *
-     * @param request The Http request
-     * @return the html code of the agenda form
-     */
-    @View( VIEW_CREATE_AGENDA )
-    public XPage getCreateAgenda( HttpServletRequest request )
-    {
-        _agenda = ( _agenda != null ) ? _agenda : new Agenda(  );
-
-        Map<String, Object> model = getModel(  );
-        model.put( MARK_AGENDA, _agenda );
-           
-        return getXPage( TEMPLATE_CREATE_AGENDA, request.getLocale(  ), model );
-    }
-
-    /**
-     * Process the data capture form of a new agenda
-     *
-     * @param request The Http Request
-     * @return The Jsp URL of the process result
-     */
-    @Action( ACTION_CREATE_AGENDA )
-    public XPage doCreateAgenda( HttpServletRequest request )
-    {
-        populate( _agenda, request );
-
-        // Check constraints
-        if ( !validateBean( _agenda, getLocale( request ) ) )
-        {
-            return redirectView( request, VIEW_CREATE_AGENDA );
-        }
-
-        AgendaHome.create( _agenda );
-        addInfo( INFO_AGENDA_CREATED, getLocale( request ) );
-
-        return redirectView( request, VIEW_MANAGE_AGENDAS );
-    }
-
-    /**
-     * Manages the removal form of a agenda whose identifier is in the http
-     * request
-     *
-     * @param request The Http request
-     * @return the html code to confirm
-     */
-    @Action( ACTION_CONFIRM_REMOVE_AGENDA )
-    public XPage getConfirmRemoveAgenda( HttpServletRequest request ) throws SiteMessageException
-    {
-        int nId = Integer.parseInt( request.getParameter( PARAMETER_ID_AGENDA ) );
-        UrlItem url = new UrlItem( JSP_PAGE_PORTAL );
-        url.addParameter( PARAM_PAGE, MARK_AGENDA );
-        url.addParameter( PARAM_ACTION, ACTION_REMOVE_AGENDA );
-        url.addParameter( PARAMETER_ID_AGENDA, nId );
-        
-        SiteMessageService.setMessage(request, MESSAGE_CONFIRM_REMOVE_AGENDA, SiteMessage.TYPE_CONFIRMATION, url.getUrl(  ));
-        return null;
-    }
-
-    /**
-     * Handles the removal form of a agenda
-     *
-     * @param request The Http request
-     * @return the jsp URL to display the form to manage agendas
-     */
-    @Action( ACTION_REMOVE_AGENDA )
-    public XPage doRemoveAgenda( HttpServletRequest request )
-    {
-        int nId = Integer.parseInt( request.getParameter( PARAMETER_ID_AGENDA ) );
-        AgendaHome.remove( nId );
-        addInfo( INFO_AGENDA_REMOVED, getLocale( request ) );
-
-        return redirectView( request, VIEW_MANAGE_AGENDAS );
-    }
-
-    /**
-     * Returns the form to update info about a agenda
-     *
-     * @param request The Http request
-     * @return The HTML form to update info
-     */
-    @View( VIEW_MODIFY_AGENDA )
-    public XPage getModifyAgenda( HttpServletRequest request )
-    {
-        int nId = Integer.parseInt( request.getParameter( PARAMETER_ID_AGENDA ) );
-
-        if ( _agenda == null  || ( _agenda.getId( ) != nId ))
-        {
-            _agenda = AgendaHome.findByPrimaryKey( nId );
-        }
-
-        Map<String, Object> model = getModel(  );
-        model.put( MARK_AGENDA, _agenda );
-        
-        return getXPage( TEMPLATE_MODIFY_AGENDA, request.getLocale(  ), model );
-    }
-
-    /**
-     * Process the change form of a agenda
-     *
-     * @param request The Http request
-     * @return The Jsp URL of the process result
-     */
-    @Action( ACTION_MODIFY_AGENDA )
-    public XPage doModifyAgenda( HttpServletRequest request )
-    {
-        populate( _agenda, request );
-
-        // Check constraints
-        if ( !validateBean( _agenda, getLocale( request ) ) )
-        {
-            return redirect( request, VIEW_MODIFY_AGENDA, PARAMETER_ID_AGENDA, _agenda.getId( ) );
-        }
-
-        AgendaHome.update( _agenda );
-        addInfo( INFO_AGENDA_UPDATED, getLocale( request ) );
-
-        return redirectView( request, VIEW_MANAGE_AGENDAS );
-    }
-    
-    
-    
     @View( value = VIEW_AGENDA_EVENTS )
     public XPage getAgendaEvents( HttpServletRequest request )
     {           
         int nId = Integer.parseInt( request.getParameter( PARAMETER_ID_AGENDA ) );
 
-        if ( _agenda == null  || ( _agenda.getId( ) != nId ))
-        {
-            _agenda = AgendaHome.findByPrimaryKey( nId );
-        }
+        Agenda agenda = AgendaHome.findByPrimaryKey( nId );
 
         Map<String, Object> model = getModel(  );
-        model.put( MARK_AGENDA, _agenda );
+        model.put( MARK_AGENDA, agenda );
         return getXPage( TEMPLATE_AGENDA_EVENTS, request.getLocale(  ), model);
     }
-    
 }
